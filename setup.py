762bbc23d521868d4d7137a6b626af290c851997
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-06-08 05:54:12 +0000

    af2cf3de5145a0bc0b378cd6eb16284a20b37195

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "tator"
-VERSION = "0.0.5"
+VERSION = "0.0.6"
 # To install the library, run the following
 #
 # python setup.py install
--- tator/configuration.py
+++ tator/configuration.py
@@ -381,7 +381,7 @@ conf = tator.Configuration(
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: v1\n"\
-               "SDK Package Version: 0.0.5".\
+               "SDK Package Version: 0.0.6".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
