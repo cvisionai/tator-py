50e027013a7ed99b288e1686686b265d0d453681
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-06-08 22:17:14 +0000

    Get version from codegen module

--- tator/__init__.py
+++ tator/__init__.py
@@ -3,3 +3,4 @@ from .openapi import *
 # Only import functions that do not have additional dependencies to top level module.
 from .util.get_api import get_api
 from .util.get_parser import get_parser
+from .version import __version__ as __version__
---
+++ tator/version.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+import tator.openapi.tator_openapi
+__version__ = tator.openapi.tator_openapi.__version__
+
+if __name__ == '__main__':
+    print(__version__)
