5d9e9a52b6dd1344b1caff42463ea0f652f7dbd7
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-06-08 05:11:28 +0000

    821ce251e3c4531fc3b83a2b21fdc7354306e397

--- tator/configuration.py
+++ tator/configuration.py
@@ -247,5 +247,5 @@ class Configuration(six.with_metaclass(TypeWithDefault, object)):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: v1\n"\
-               "SDK Package Version: 0.0.4".\
+               "SDK Package Version: 0.0.5".\
                format(env=sys.platform, pyversion=sys.version)
