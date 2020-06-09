267959840487801fe102d58aeb154b208087f0f0
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-06-09 19:46:17 +0000

    Add utility function to upload temporary files

--- tator/util/__init__.py
+++ tator/util/__init__.py
@@ -1,3 +1,4 @@
 from .upload_media import upload_media
 from .upload_media_archive import upload_media_archive
+from .upload_temporary_file import upload_temporary_file
 from .chunked_create import chunked_create
