8c06a101ba0e0e82153a56b767088ae99ed7eb09
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-06-09 20:36:03 +0000

    Import `download_media` to root module level

--- tator/util/__init__.py
+++ tator/util/__init__.py
@@ -1,4 +1,5 @@
 from .upload_media import upload_media
 from .upload_media_archive import upload_media_archive
 from .upload_temporary_file import upload_temporary_file
+from .download_media import download_media
 from .chunked_create import chunked_create
