44a6cd3fbf82c339f0611b86b2a1643ea274659d
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-06-10 04:43:31 +0000

    Update version test

--- test/test_version.py
+++ test/test_version.py
@@ -1,24 +1,28 @@
-import pytator
+import tator
 
 def test_version_crud(url, token, project):
-    tator = pytator.Tator(url, token, project)
+    tator_api = tator.get_api(url, token)
 
     # Test single create.
-    status, response = tator.Version.new({
+    response = tator_api.create_version(project, version_spec={
         'name': 'Test Version',
         'description': 'A version for testing',
     })
-    pk = response['id']
-    assert status == 201
+    assert isinstance(response, tator.CreateResponse)
+    print(response.message)
+    pk = response.id
 
     # Test patch.
-    status, response = tator.Version.update(pk, {'name': 'Updated Version'})
-    assert status == 200
+    response = tator_api.update_version(pk, version_update={'name': 'Updated Version'})
+    assert isinstance(response, tator.MessageResponse)
+    print(response.message)
 
     # Compare with get results.
-    updated = tator.Version.get(pk)
-    assert updated['name'] == 'Updated Version'
+    updated = tator_api.get_version(pk)
+    assert isinstance(updated, tator.Version)
+    assert updated.name == 'Updated Version'
 
     # Test delete.
-    status = tator.Version.delete(pk)
-    assert status == 200
+    response = tator_api.delete_version(pk)
+    assert isinstance(response, tator.MessageResponse)
+    print(response.message)
