8adc82102d5d900d36c4cc28fb97f39af205ef80
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-06-10 01:44:27 +0000

    Remove download_media from top level imports since requests is not part of base requirements

--- tator/__init__.py
+++ tator/__init__.py
@@ -3,5 +3,4 @@ from .openapi import *
 # Only import functions that do not have additional dependencies to top level module.
 from .util.get_api import get_api
 from .util.get_parser import get_parser
-from .util.download_media import download_media
 from .version import __version__ as __version__
