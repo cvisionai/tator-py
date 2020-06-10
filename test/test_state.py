0f8cbe8c7dc86b9b54da5cc1047308af6a900322
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-06-10 05:14:29 +0000

    Update state tests

--- test/test_state.py
+++ test/test_state.py
@@ -3,7 +3,9 @@ import random
 import uuid
 import time
 
-import pytator
+import tator
+from tator.util import chunked_create
+from tator.util import to_dataframe
 from ._common import assert_close_enough
 
 def random_state(project, state_type, video_obj, post=False):
@@ -19,8 +21,8 @@ def random_state(project, state_type, video_obj, post=False):
     out = {
         'project': project,
         'type': state_type,
-        'media_ids': [video_obj['id']],
-        'frame': random.randint(0, video_obj['num_frames'] - 1),
+        'media_ids': [video_obj.id],
+        'frame': random.randint(0, video_obj.num_frames - 1),
     }
     if post:
         out = {**out, **attributes}
@@ -29,8 +31,8 @@ def random_state(project, state_type, video_obj, post=False):
     return out
 
 def test_state_crud(url, token, project, video_type, video, state_type):
-    tator = pytator.Tator(url, token, project)
-    video_obj = tator.Media.get(pk=video)
+    tator_api = tator.get_api(url, token)
+    video_obj = tator_api.get_media(video)
 
     # These fields will not be checked for object equivalence after patch.
     exclude = ['project', 'type', 'media_ids', 'id', 'meta', 'user', 'frame']
@@ -41,28 +43,33 @@ def test_state_crud(url, token, project, video_type, video, state_type):
         random_state(project, state_type, video_obj, post=True)
         for _ in range(num_states)
     ]
-    status, response = tator.State.new(states)
-    print(f"New state response: {response}")
-    assert status == 201
+    state_ids = chunked_create(tator_api.create_state_list,
+                               project, state_spec=states)
+    assert len(state_ids) == len(states)
+    print(f"Created {len(state_ids)} states!")
 
     # Test single create.
     state = random_state(project, state_type, video_obj, post=True)
-    status, response = tator.State.new([state])
-    state_id = response['id'][0]
-    assert status == 201
+    response = tator_api.create_state_list(project, state_spec=[state])
+    assert isinstance(response, tator.CreateListResponse)
+    print(response.message)
+    state_id = response.id[0]
 
     # Patch single state.
     patch = random_state(project, state_type, video_obj)
-    status, response = tator.State.update(state_id, patch)
-    assert status == 200
+    response = tator_api.update_state(state_id, state_update=patch)
+    assert isinstance(response, tator.MessageResponse)
+    print(response.message)
 
     # Get single state.
-    updated_state = tator.State.get(state_id)
+    updated_state = tator_api.get_state(state_id)
+    assert isinstance(updated_state, tator.State)
     assert_close_enough(patch, updated_state, exclude)
     
     # Delete single state.
-    status = tator.State.delete(state_id)
-    assert status == 200
+    response = tator_api.delete_state(state_id)
+    assert isinstance(response, tator.MessageResponse)
+    print(response.message)
 
     # ES can be slow at indexing so wait for a bit.
     time.sleep(5)
@@ -70,20 +77,22 @@ def test_state_crud(url, token, project, video_type, video, state_type):
     # Bulk update state attributes.
     bulk_patch = random_state(project, state_type, video_obj)
     bulk_patch = {'attributes': bulk_patch['attributes']}
-    params = {'media_id': video, 'type': state_type}
-    status, response = tator.State.bulk_update(params, bulk_patch)
-    assert status == 200
+    params = {'media_id': [video], 'type': state_type}
+    response = tator_api.update_state_list(project, **params,
+                                           attribute_bulk_update=bulk_patch)
+    assert isinstance(response, tator.MessageResponse)
+    print(response.message)
 
     # Verify all states have been updated.
-    states = tator.State.filter(params)
+    states = tator_api.get_state_list(project, **params)
     for state in states:
         assert_close_enough(bulk_patch, state, exclude)
     
     # Delete all state.
-    status = tator.State.bulk_delete(params)
-    assert status == 200
+    status = tator_api.delete_state_list(project, **params)
+    assert isinstance(response, tator.MessageResponse)
     time.sleep(1)
 
     # Verify all states are gone.
-    states = tator.State.filter(params)
+    states = tator_api.get_state_list(project, **params)
     assert states == []
