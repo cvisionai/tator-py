e4e87d3a58623939746cfff509bf26b72eb88fbb
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-06-11 13:20:17 +0000

    Yield responses in `chunked_create`.

--- tator/util/chunked_create.py
+++ tator/util/chunked_create.py
@@ -4,17 +4,17 @@ def chunked_create(func, project, **kwargs):
     Example:
     .. highlight:: python
     .. code-block:: python
-        ids = chunked_create(api.create_localization_list,
-                             1, localization_spec=my_long_list)
+        created_ids = []
+        for progress in tator.chunked_create(api.create_localization_list,
+                                             1, localization_spec=my_long_list):
+            created_ids += response.id
 
     :param func: Function to call on each chunk.
     :param project: Unique integer identifying a project.
-    :returns: List of created IDs.
+    :returns: Generator that yields a response.
     """
-    ids = []
     for key in kwargs:
         spec = kwargs[key]
     for idx in range(0, len(spec), 500):
         response = func(project, **{key: spec[idx:idx+500]})
-        ids += response.id
-    return ids
+        yield response
