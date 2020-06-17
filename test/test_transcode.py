933ff3fd29e4f8e983ab937e12f771645d6c815e
Brian Tate <brian.tate@cvisionai.com>
2020-06-17 10:13:26 -0400

    Add script to test fault handling

---
+++ test/test_transcode.py
@@ -0,0 +1,26 @@
+import tator
+from uuid import uuid1
+
+def test_transcode_fault_handling(host, token, project):
+    """ Verify Error Handling occurs for transcode endpoint """
+    tator_api = tator.get_api(host, token)
+    exception = False
+    try:
+        video_obj = tator_api.transcode(project,
+                                        transcode_spec={'gid':str(uuid1()),
+                                                        'md5':"1234",
+                                                        'name':"blah",
+                                                        'section':"blah",
+                                                        'type':200,
+                                                        'uid':str(uuid1()),
+                                                        'url':"blah"})
+    except tator.exceptions.ApiException as e:
+        exception = True
+        code = e.status
+
+    assert(exception)
+    assert(code == 400)
+
+
+    
+
