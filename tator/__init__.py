7878cab6abe206fe8337d9ff8ae1d63538a2d133
Brian Tate <brian.tate@cvisionai.com>
2020-06-18 21:53:49 -0400

    Add download media to top-level module

--- tator/__init__.py
+++ tator/__init__.py
@@ -6,6 +6,8 @@ online platform
 # Shortcuts
 from .util.get_api import get_api
 from .util.get_parser import get_parser
+from .util.download_media import download_media
+
 from .version import __version__ as __version__
 
 # Generated code imports to top-level
