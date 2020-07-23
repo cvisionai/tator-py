5475fabb69bc792d405626c1ee2b11bc9ac63f03
Brian Tate <brian.tate@cvisionai.com>
2020-07-23 08:50:13 -0400

    Pass in parser to get_parser util function

--- tator/util/get_parser.py
+++ tator/util/get_parser.py
@@ -1,11 +1,12 @@
 import argparse
 
-def get_parser():
+def get_parser(parser=None):
     """ Returns an argument parser that includes host and token.
 
     :returns: :class:`argparse.ArgumentParser` object that includes host and token arguments.
     """
-    parser = argparse.ArgumentParser(description="Creates a test project.")
+    if parser is None:
+        parser = argparse.ArgumentParser(description="Creates a test project.")
     parser.add_argument('--host', default='https://www.tatorapp.com')
     parser.add_argument('--token', help="Your API token.")
     return parser
