abde6616c00181a12195b8d80a3b497d5d4ab773
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-08-14 20:16:49 +0000

    Update stategraphic unit test with longer track

--- test/test_stategraphic.py
+++ test/test_stategraphic.py
@@ -1,4 +1,5 @@
 import tator
+import datetime
 
 from ._common import assert_vector_equal
 
@@ -15,11 +16,13 @@ def _make_box(project, box_type, video, frame):
     }
 
 def test_stategraphic(host, token, project, video, box_type, track_type):
+    TRACK_LENGTH=320
+
     tator_api = tator.get_api(host, token)
     video_obj = tator_api.get_media(video)
 
     # Make boxes for track.
-    boxes = [_make_box(project, box_type, video, frame) for frame in range(10)]
+    boxes = [_make_box(project, box_type, video, frame) for frame in range(TRACK_LENGTH)]
     response = tator_api.create_localization_list(project, localization_spec=boxes)
     assert isinstance(response, tator.models.CreateListResponse)
     print(response.message)
@@ -37,10 +40,11 @@ def test_stategraphic(host, token, project, video, box_type, track_type):
     track_id = response.id[0]
 
     # Get state graphic.
-    file_path = tator_api.get_state_graphic(track_id, mode='tile')
-    state = tator_api.get_state(track_id)
-    stategraphic = tator.util.get_images(file_path, state)
-    assert(len(stategraphic) == 10)
+    t0 = datetime.datetime.now()
+    stategraphic = tator.util.full_state_graphic(tator_api, track_id)
+    t1 = datetime.datetime.now()
+    print(f"State graphic took {t1 - t0} seconds.")
+    assert(len(stategraphic) == TRACK_LENGTH)
     for frame_data in stategraphic:
         size = (frame_data.height, frame_data.width, len(frame_data.mode))
         assert_vector_equal(size, (720,1280,3))
