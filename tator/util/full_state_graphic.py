4139caaaffff5dbf90a9abd6edb412577e53405e
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-08-14 20:17:08 +0000

    Add utility to retrieve stategraphic for entire track

---
+++ tator/util/full_state_graphic.py
@@ -0,0 +1,28 @@
+import itertools
+
+import tator
+
+def full_state_graphic(api, state_id, force_scale=''):
+    """ Retrieves all detection images for a state.
+
+    Example:
+
+    .. code-block:: python
+
+        state_graphic = full_state_graphic(state_id)
+
+    :param api: :class:`tator.TatorApi` object.
+    :param state_id: Unique integer identifying a localization-associated state.
+    :param force_scale: (Optional) wxh to force each tile prior to stitch.
+    :returns: List containing all detection images in this state.
+    """
+    BATCH_SIZE = 30
+    state = api.get_state(state_id)
+    graphics = []
+    for offset in range(0, len(state.localizations), BATCH_SIZE):
+        kwargs = dict(mode='tile', length=BATCH_SIZE, offset=offset)
+        if force_scale:
+            kwargs['force_scale'] = force_scale
+        path = api.get_state_graphic(state_id, **kwargs)
+        graphics.append(tator.util.get_images(path, state))
+    return list(itertools.chain(graphics))
