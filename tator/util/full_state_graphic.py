b3a8dd64ce89e3bf7c0f0673701826260997e6f5
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-08-18 19:37:37 +0000

    Test for default size of 224x224

--- tator/util/full_state_graphic.py
+++ tator/util/full_state_graphic.py
@@ -2,7 +2,7 @@ import itertools
 
 import tator
 
-def full_state_graphic(api, state_id, force_scale=''):
+def full_state_graphic(api, state_id, force_scale=None):
     """ Retrieves all detection images for a state.
 
     Example:
@@ -21,7 +21,7 @@ def full_state_graphic(api, state_id, force_scale=''):
     graphics = []
     for offset in range(0, len(state.localizations), BATCH_SIZE):
         kwargs = dict(mode='tile', length=BATCH_SIZE, offset=offset)
-        if force_scale:
+        if force_scale is not None:
             kwargs['force_scale'] = force_scale
         path = api.get_state_graphic(state_id, **kwargs)
         num_images = len(state.localizations[offset:offset+BATCH_SIZE])
