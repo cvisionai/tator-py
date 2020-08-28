886b323fd42e01c479c753a6cd14b9d5c858f298
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-08-28 20:45:13 +0000

    Add unit test for erroneous file input for local transcodes and subsequent media object deletion

--- test/test_local_transcode.py
+++ test/test_local_transcode.py
@@ -11,3 +11,18 @@ def test_local_transcode(host, token, project, video_type, video_file):
         '--section', 'Locally transcoded',
     ]
     subprocess.run(cmd, check=True)
+
+def test_bad_file(host, token, project, video_type, image_file):
+    cmd = [
+        'python3', '-m', 'tator.transcode', image_file,
+        '--host', host,
+        '--token', token,
+        '--project', str(project),
+        '--type', str(video_type),
+        '--section', 'Bad transcodes',
+    ]
+    subprocess.run(cmd, check=True)
+    # Make sure media file is gone.
+    api = tator.get_api(host, token)
+    medias = api.get_media_list(project, attribute='tator_user_sections::Bad transcodes')
+    assert(len(medias) == 0)
