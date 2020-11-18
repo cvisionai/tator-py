6477cb45a192d0abdb3802feb47754a8a00aed69
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-11-18 20:55:40 +0000

    Implement HostTransfer class for moving files between hosts

--- tator/util/clone_media_list.py
+++ tator/util/clone_media_list.py
@@ -1,11 +1,59 @@
 import os
 import tempfile
+from uuid import uuid1
+from collections import defaultdict
 
 from urllib.parse import urljoin
 
+from .md5sum import md5sum
 from ._download_file import _download_file
 
-def clone_media_list(src_api, query_params, dest_project, dest_section='', dest_media_type=-1,
+class HostTransfer:
+    def __init__(self, src_api, dest_api):
+        """ Sets up authentication for transferring file.
+
+        :param src_api: :class:`tator.TatorApi` object corresponding to source host.
+        :param dest_api: :class:`tator.TatorApi` object corresponding to destination host.
+        """
+        # Set up source auth.
+        config = src_api.api_client.configuration
+        token = config.api_key['Authorization']
+        prefix = config.api_key_prefix['Authorization']
+        self.src_host = config.host
+        self.src_headers = {
+            'Authorization': f'{prefix} {token}',
+            'Content-Type': f'application/json',
+            'Accept-Encoding': 'gzip',
+        }
+
+        # Set up destination auth.
+        config = dest_api.api_client.configuration
+        self.dest_token = config.api_key['Authorization']
+        self.dest_prefix = config.api_key_prefix['Authorization']
+        self.dest_host = config.host
+        self.tus_url = urljoin(self.dest_host, 'files/')
+
+    def __call__(self, src_url):
+        """ Downloads a file from one host and uploads it to another.
+        
+        :param src_url: Relative URL of source file to download.
+        :returns: URL of destination file.
+        """
+        url = urljoin(self.src_host, src_url)
+        upload_uid = str(uuid1())
+        tus_headers = {'Authorization': f'{self.dest_prefix} {self.dest_token}',
+                       'Upload-Uid': f'{upload_uid}'}
+        tus = TusClient(self.tus_url, headers=tus_headers)
+        with tempfile.NamedTemporaryFile() as temp:
+            _download_file(url, self.src_headers, temp.name)
+            uploader = tus.uploader(temp.name, chunk_size=chunk_size,
+                                    retries=10, retry_delay=15)
+            num_chunks = math.ceil(uploader.get_file_size()/chunk_size)
+            for chunk_count in range(num_chunks):
+                uploader.upload_chunk()
+        return uploader.url
+
+def clone_media_list(src_api, query_params, dest_project, dest_media_type=-1, dest_section='', 
                      dest_api=None):
     """ Clone media list.
 
@@ -47,12 +95,15 @@ def clone_media_list(src_api, query_params, dest_project, dest_section='', dest_
             created_ids += response.id # This response is from the CloneMedia endpoint.
         print(f"Finished creating {num_created} files!")
 
-    :param api: :class:`tator.TatorApi` object.
+    :param src_api: :class:`tator.TatorApi` object corresponding to source host or only
+        host if this is a clone on same host.
     :param query_params: Dictionary containing query parameters for source media list.
     :param dest_project: Unique integer identifying destination project.
     :param dest_media_type: Unique integer identifying destination media type. If set to
         -1, the media type is set to the first media type in the project found with
         the proper dtype for the files.
+    :param dest_section: Name of destination section.
+    :param dest_api: :class:`tator.TatorApi` object corresponding to destination host.
     :returns: Generator containing number of files created, number of files total, and
         most recent response from media creation operation.
     """
@@ -69,16 +120,6 @@ def clone_media_list(src_api, query_params, dest_project, dest_section='', dest_
     if 'project' not in query_params:
         raise Exception("Query parameters must include a project!")
 
-    # Get auth info for downloading media.
-    host = config.host
-    token = config.api_key['Authorization']
-    prefix = config.api_key_prefix['Authorization']
-    headers = {
-        'Authorization': f'{prefix} {token}',
-        'Content-Type': f'application/json',
-        'Accept-Encoding': 'gzip',
-    }
-
     # Guess the media type if it was not given.
     if dest_media_type == -1:
         mime,_ = mimetypes.guess_type(fname)
@@ -92,16 +133,19 @@ def clone_media_list(src_api, query_params, dest_project, dest_section='', dest_
     # Clone the media.
     created_ids = []
     after = None
+    query_params['stop'] = 500
+    query_params['start'] = 0
     while len(created_ids) < total_files:
-        if after is None:
-            query_params['start'] = 0
-        else:
-            if 'start' in query_params:
-                query_params.pop('start', None)
-            query_params['after'] = after
+        # Get paginated medias.
+        medias = src_api.get_media_list(**query_params)
+        # Set after parameter for next iteration.
+        query_params['after'] = medias[-1].name
+        if 'start' in query_params:
+            query_params.pop('start', None)
         if dest_api is None:
             # Clone media locally.
-            response = src_api.clone_media_list(**query_params, clone_spec={
+            media_ids = [media.id for media in medias]
+            response = src_api.clone_media_list(media_id=media_ids, clone_spec={
                 'dest_project': dest_project,
                 'dest_section': dest_section,
                 'dest_media_type': dest_media_type,
@@ -110,57 +154,50 @@ def clone_media_list(src_api, query_params, dest_project, dest_section='', dest_
             yield (len(created_ids), total_files, response)
         else:
             # Clone media to another host.
-            medias = src_api.get_media_list(**query_params)
-            
+            transfer = HostTransfer(src_api, dest_api)
             for media in medias:
-                with tempfile.TemporaryDirectory() as temp_dir:
-                    # Download media to a temporary directory. 
-                    media_paths = defaultdict(list)
-                    def _download_to_temp(path, key):
-                        url = urljoin(host, path)
-                        out_path = os.path.join(temp_dir, os.path.basename(path))
-                        _download_file(url, headers, out_path)
-                        media_paths[key].append(out_path)
-                    if media.media_files:
-                        if media.media_files.archival:
-                            for archival in media.media_files.archival:
-                                _download_to_temp(archival.path, 'archival')
-                        if media.media_files.streaming:
-                            for streaming in media.media_files.streaming:
-                                _download_to_temp(streaming.path, 'streaming')
-                        if media.media_files.audio:
-                            for audio in media.media_files.audio:
-                                _download_to_temp(audio.path, 'audio')
-                    if media.file:
-                        _download_to_temp(media.file, 'file')
-                    if media.original:
-                        _download_to_temp(media.original, 'archival')
 
-                    # Upload media.
-                    for key in media_paths:
-                        tus_url = urljoin(host, 'files/')
-    tusURL = urljoin(host, "files/")
-                        tus = TusClient(tus_url, headers={'Authorization': f'{prefix} {token}',
-                                                          'Upload-Uid': f'{upload_uid}'})
-                        uploader = tus.uploader(path, chunk_size=chunk_size,
-                                                retries=10, retry_delay=15)
-                        num_chunks = math.ceil(uploader.get_file_size()/chunk_size)
-                        for chunk_count in range(num_chunks):
-                            uploader.upload_chunk()
-                        spec = {
-                            'type': type_id,
-                            'uid': upload_uid,
-                            'gid': upload_gid,
-                            'url': uploader.url,
-                            'name': fname,
-                            'section': section,
-                            'md5': md5,
-                            'attributes': attributes,
-                            'media_id': media_id,
-                        }
-                        # Initiate transcode or save image.
-                        if mime.find('video') >= 0:
-                            response = api.transcode(project_id, transcode_spec=spec)
-                        else:
-                            response = api.create_media(project_id, media_spec=spec)
-                        yield (100, response)
+                # Set up media spec.
+                media_spec = {
+                    'type': media_type,
+                    'name': media.name,
+                    'gid': media.gid,
+                    'uid': media.uid,
+                    'md5': media.md5,
+                    'attributes': media.attributes,
+                    'section': dest_section if dest_section else media.section,
+                }
+                # Transfer thumbnails and image.
+                media_spec['thumbnail_url'] = transfer(f'/media/{media.thumbnail}')
+
+                if media.thumbnail_gif:
+                    media_spec['thumbnail_gif_url'] = transfer(f'/media/{media.thumbnail_gif}')
+
+                if media.file:
+                    media_spec['file'] = transfer(f'/media/{media.file}')
+
+                # Create the media object.
+                response = dest_api.create_media(dest_project, media_spec=media_spec)
+
+                # Transfer videos.
+                if media.media_files:
+                    if media.media_files.archival:
+                        for archival in media.media_files.archival:
+                            media_def = archival.to_dict()
+                            media_def.pop('path', None)
+                            media_def['url'] = transfer(archival.path)
+                            dest_api.move_video(media.id, {'archival': [media_def]})
+                    if media.media_files.streaming:
+                        for streaming in media.media_files.streaming:
+                            media_def = streaming.to_dict()
+                            media_def.pop('path', None)
+                            media_def['url'] = transfer(streaming.path)
+                            media_def['segments_url'] = transfer(streaming.segment_info)
+                            dest_api.move_video(media.id, {'streaming': [media_def]})
+                    if media.media_files.audio:
+                        for audio in media.media_files.audio:
+                            media_def = audio.to_dict()
+                            media_def.pop('path', None)
+                            media_def['url'] = transfer(audio.path)
+                            dest_api.move_video(media.id, {'audio': [media_def]})
+
