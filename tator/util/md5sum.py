1443b377b1f8b3dde39d5324f769a42f32487a84
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-12-17 17:03:33 +0000

    Update md5sum function so that it matches JS: Only compute on first 100MB and salt in file size.

--- tator/util/md5sum.py
+++ tator/util/md5sum.py
@@ -1,13 +1,23 @@
 import hashlib
+import os
 
-def md5sum(fname):
+def md5sum(fname, size=None):
     """ Computes md5 sum of a file contents.
 
     :param fname: Path to file.
+    :param size: Size of file. Will be computed if not given.
     :returns: md5 sum of the file.
     """
+    CHUNK_SIZE = 100*1024*1024 # 100MB
     md5 = hashlib.md5()
     with open(fname, 'rb') as f:
-        for chunk in iter(lambda: f.read(128*md5.block_size), b''):
+        for chunk in iter(lambda: f.read(CHUNK_SIZE), b''):
             md5.update(chunk)
-    return md5.hexdigest()
+            break # Exit after one chunk
+    # Compute file size if not given.
+    if size is None:
+        size = os.stat(fname).st_size
+    # Salt in file size.
+    out = hashlib.md5()
+    out.update(md5.hexdigest().encode('utf-8') + str(size).encode('utf-8'))
+    return out.hexdigest()
