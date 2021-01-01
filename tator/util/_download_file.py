cd45fd17f7508b34842f9d051f439412ce7afaa5
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2021-01-01 16:02:15 +0000

    Remove debug statements

--- tator/util/_download_file.py
+++ tator/util/_download_file.py
@@ -11,7 +11,6 @@ def _download_file(api, project, url, out_path):
     MAX_RETRIES = 10
     # If this is a normal url, get headers.
     if url.startswith('/'):
-        logger.warning("DOWNLOAD WITH AUTHENTICATION")
         config = api.api_client.configuration
         host = config.host
         token = config.api_key['Authorization']
@@ -24,11 +23,9 @@ def _download_file(api, project, url, out_path):
             'Accept-Encoding': 'gzip',
         }
     elif url.startswith('http'):
-        logger.warning("NORMAL DOWNLOAD NO HEADERS")
         headers = {}
     # If this is a S3 object key, get a download url.
     else:
-        logger.warning("RETRIEVING S3 OBJECT")
         url = api.get_download_info(project, {'keys': [url]})[0].url
         headers = {}
     for attempt in range(MAX_RETRIES):
