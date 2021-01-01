9ab1f0cc834f8967844704322a843cbab23440cb
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2021-01-01 15:19:00 +0000

    Add test for image import

--- test/test_import_media.py
+++ test/test_import_media.py
@@ -1,9 +1,14 @@
 import tator
 import time
 
-def test_import_media(host, token, project, video_type):
+def test_import_image(host, token, project, image_type):
     api = tator.get_api(host, token)
+    url = 'https://upload.wikimedia.org/wikipedia/en/7/7d/Lenna_%28test_image%29.png'
+    response = tator.util.import_media(api, image_type, url)
+    assert(isinstance(response, tator.models.CreateResponse))
 
+def test_import_video(host, token, project, video_type):
+    api = tator.get_api(host, token)
     url = 'http://www.ballastmedia.com/wp-content/uploads/AudioVideoSyncTest_BallastMedia.mp4'
     response = tator.util.import_media(api, video_type, url)
     print(response.message)
@@ -16,3 +21,4 @@ def test_import_media(host, token, project, video_type):
         else:
             print("Waiting for transcode of imported media to complete...")
             time.sleep(2.5)
+
