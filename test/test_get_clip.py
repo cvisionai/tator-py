4b3be4ed0445d7c763a6843ae24780c4fda0ca0b
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2021-03-19 14:07:06 +0000

    Add unit test for `get_clip`

---
+++ test/test_get_clip.py
@@ -0,0 +1,16 @@
+import os
+import tempfile
+
+import tator
+
+def test_get_clip(host, token, project, video):
+    tator_api = tator.get_api(host, token)
+    video_obj = tator_api.get_media(video)
+
+    frame_ranges = ['0:100']
+    clip = tator_api.get_clip(video, frame_ranges=frame_ranges)
+    with tempfile.TemporaryDirectory() as td:
+        video_path = os.path.join(td, 'video_clip_test.mp4')
+        for progress in tator.util.download_temporary_file(tator_api, clip.file, video_path):
+            print(f"Video clip download progress: {progress}%")
+
