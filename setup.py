3da5dd3944754624cc92a9b10e1ad46455286d9e
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2021-04-21 13:30:22 +0000

    Add pillow to required dependencies (else `import tator` fails due to `make_multi_stream`)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import yaml
 import json
 
 REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil",
-            "requests", "pyyaml", "progressbar2"]
+            "requests", "pyyaml", "progressbar2", "pillow"]
 
 SCHEMA_FILENAME = 'schema.yaml'
 CONFIG_FILENAME = 'config.json'
