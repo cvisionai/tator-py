2899a65f5d07923957f72668c3cc2199318d3d12
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2021-05-25 20:29:07 +0000

    Update platform stats script for readability, display stats by project and org

--- examples/platform_stats.py
+++ examples/platform_stats.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import tator
 
 if __name__ == '__main__':
@@ -7,9 +9,18 @@ if __name__ == '__main__':
     projects = api.get_project_list()
     total_size = 0
     total_duration = 0
+    size_by_org = defaultdict(int)
+    duration_by_org = defaultdict(int)
     for project in projects:
         stats = api.get_media_stats(project.id)
+        print(f"Project {project.id} ({project.name}): {stats.total_size / 1e12:0.2f}TB, {stats.duration / 3600:0.2f} hours")
+        size_by_org[project.organization] += stats.total_size
+        duration_by_org[project.organization] += stats.duration
         total_size += stats.total_size
         total_duration += stats.duration
-    print(f"Total size: {total_size} bytes")
-    print(f"Total duration: {total_duration} seconds")
+    print(f"-----------------------------------------------------------")
+    for org in size_by_org.keys():
+        print(f"Organization {org}: {size_by_org[org] / 1e12:0.2f}TB, {duration_by_org[org] / 3600:0.2f} hours")
+    print(f"-----------------------------------------------------------")
+    print(f"Total size: {total_size / 1e12:0.2f} TB")
+    print(f"Total duration: {total_duration / 3600:0.2f} hours")
