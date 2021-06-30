1643e0df9131f719ef22aa76eaef5f12d698c768
Hugh Enxing <hugh.enxing@cvisionai.com>
2021-06-30 16:29:12 -0400

    Allows for extant destination sections

--- examples/move_to_new_section.py
+++ examples/move_to_new_section.py
@@ -30,7 +30,7 @@ project_id = 123
 # Source section, must already exist
 source_section = "Source Section Name"
 
-# Destination section, must not exist yet
+# Destination section, may already exist
 dest_section = "New Section Name"
 
 # +
@@ -42,12 +42,13 @@ about the source section and affected media.
 """
 
 # Get tator api with credentials
-common_err = "fix the first cell and start over"
+common_err = "please fix the first cell and start over"
 import tator
+
 try:
     api = tator.get_api(host=host, token=token)
 except:
-    print(f"Could not access tator at '{host}' with token '{token}', {common_err}")
+    print(f"Could not get api at '{host}' with token '{token}', {common_err}")
     raise
 
 # Test project access
@@ -62,20 +63,14 @@ response = api.get_section_list(project=project_id, name=source_section)
 assert len(response) == 1, f"No unique section matching the name '{source_section}', {common_err}"
 source_section_id = response[0].id
 
-# Make sure dest section does not exist
-response = api.get_section_list(project=project_id, name=dest_section)
-assert len(response) == 0, f"Found at least one section matching the name '{dest_section}', {common_err}"
-
-# Get media list from source section
-media_ids = [m.id for m in api.get_media_list(project=project_id, section=source_section_id)]
+# Get media count from source section
+media_count = api.get_media_count(project=project_id, section=source_section_id)
 
 print(
-    f"Accessed project {project_id} successfully, "
-    f"source section '{source_section}' has id {source_section_id} and {len(media_ids)} media, "
-    f"found no extant section with the name '{dest_section}'"
+    f"Accessed project {project_id} successfully, source section '{source_section}' has id {source_section_id} and {media_count} media"
 )
 
-print(f"Running the next cell will move the following media to the new section:\n{media_ids}")
+print("Running the next cell will move media to the new section!")
 
 # +
 """
@@ -85,25 +80,53 @@ BE RUN ONCE after all preceeding cells have run without exception.
 """
 from uuid import uuid4
 
+# Create or find dest section
+response = api.get_section_list(project=project_id, name=dest_section)
+n_sections = len(response)
+
+if n_sections > 1:
+    raise ValueError(
+        f"Found more than one section associated with name '{dest_section}', try another name"
+    )
+
+if n_sections == 1:
+    new_user_section = response[0].tator_user_sections
+    if not response[0].visible:
+        api.update_section(id=new_section_id, section_update={"visible": True})
+else:
+    new_user_section = str(uuid4())
+    section_spec = {"name": dest_section, "visible": True, "tator_user_sections": new_user_section}
+    api.create_section(project=project_id, section_spec=section_spec)
+
 # Create media bulk update spec
 try:
     media_bulk_update = {
-        "ids": media_ids,
-        "attributes": {"tator_user_sections": new_user_section} #, "original_section": source_section},
+        "attributes": {"tator_user_sections": new_user_section},
     }
 except NameError:
     print(
         "A variable from the previous cell is not set, please run it before trying to run this cell again"
     )
-
-# Create dest section
-new_user_section = str(uuid4())
-section_spec = {"name": dest_section, "visible": True, "tator_user_sections": new_user_section}
-response = api.create_section(project=project_id, section_spec=section_spec)
-dest_section_id = response.id
+    raise
 
 # Move media to dest section
-response = api.update_media_list(project=project_id, media_bulk_update=media_bulk_update)
+response = api.update_media_list(
+    project=project_id, section=source_section_id, media_bulk_update=media_bulk_update
+)
+print(response)
 
 # Archive source section
-api.update_section(id=source_section_id, section_update={"visible": False})
+response = api.update_section(id=source_section_id, section_update={"visible": False})
+# -
+
+"""
+To determine the source section of a given media entity:
+
+1. Get changelog for media in question
+2. Find entry where the `tator_user_sections` attribute changed
+3. Note the old value of it
+4. Get the list of sections in the project
+5. Find the section in the list of sections whose `tator_user_sections` value matches the value from
+   line 3
+6. Look at its name
+"""
