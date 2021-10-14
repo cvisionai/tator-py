a06fa991b1f04bf235231f1968c74f28fc54a1f0
Mark Taipan <mark.taipan@cvisionai.com>
2021-10-14 21:10:18 +0000

    Remove report utilities

--- tator/util/__init__.py
+++ tator/util/__init__.py
@@ -20,7 +20,6 @@ from tator.util.to_dataframe import to_dataframe
 from tator.util.register_algorithm import register_algorithm
 from tator.util.register_dashboard import register_dashboard
 from tator.util.update_dashboard import update_dashboard
-from tator.util.register_report import register_report
 from tator.util.clone_media_list import clone_media_list
 from tator.util.clone_localization_list import clone_localization_list
 from tator.util.clone_state_list import clone_state_list
