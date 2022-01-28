c4b5e3793cbb55aadbe2f8a44f43c762b52c88c8
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2022-01-28 23:16:35 +0000

    Allow for setting description in `get_parser`

--- tator/util/get_parser.py
+++ tator/util/get_parser.py
@@ -1,12 +1,12 @@
 import argparse
 
-def get_parser(parser=None):
+def get_parser(parser=None, description=None):
     """ Returns an argument parser that includes host and token.
 
     :returns: :class:`argparse.ArgumentParser` object that includes host and token arguments.
     """
     if parser is None:
-        parser = argparse.ArgumentParser(description="Creates a test project.")
+        parser = argparse.ArgumentParser(description=description if description else "A tator-py utility.")
     parser.add_argument('--host', default='https://www.tatorapp.com')
     parser.add_argument('--token', help="Your API token.")
     return parser
