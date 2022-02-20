d2ac68c633d62beddc2fe93a318d9051870d388c
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2022-02-20 04:28:21 +0000

    In the leaf example test, use the constructed leaf hierarchy on the string field

--- test/examples/test_leaf.py
+++ test/examples/test_leaf.py
@@ -2,7 +2,7 @@ import subprocess
 
 import tator
 
-def test_localizations(host, token, project, leaf_type):
+def test_leaf(host, token, project, leaf_type, video_type):
     cmd = [
         'python3',
         'examples/leaf.py',
@@ -12,3 +12,16 @@ def test_localizations(host, token, project, leaf_type):
         '--input', 'examples/leaf.yaml',
     ]
     subprocess.run(cmd, check=True)
+    # Apply the label tree to string fields for media types
+    api = tator.get_api(host, token)
+    api.rename_attribute(video_type, {
+        'entity_type': 'MediaType',
+        'global': 'true',
+        'old_attribute_type_name': 'test_string',
+        'new_attribute_type': {
+            'name': 'test_string',
+            'dtype': 'string',
+            'autocomplete': {'serviceUrl': f'/rest/Leaves/Suggestion/GoT/{project}'}
+        },
+    })
+    
