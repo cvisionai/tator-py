f6fcb956bc3cfa3c75b29a85f51ebc88c02bd452
Hugh Enxing <hugh.enxing@cvisionai.com>
2022-07-15 18:37:25 -0400

    Adds test that should pass

--- test/test_attributes.py
+++ test/test_attributes.py
@@ -22,3 +22,10 @@ def test_string_attributes_bulk_update(host, token, project, video_type, video):
     video_obj = tator_api.get_media(video)
     assert video_obj.attributes["test_string"] == media_bulk_update["attributes"]["test_string"]
 
+def test_enum_attributes_count_mismatch(host, token, project, state_type, video):
+    tator_api = tator.get_api(host, token)
+    media_bulk_update = {"attributes": {"test_enum": "a"}, "ids": [video]}
+    tator_api.update_media_list(project, media_bulk_update)
+    video_obj = tator_api.get_media(video)
+    assert video_obj.attributes["test_enum"] == media_bulk_update["attributes"]["test_enum"]
+    assert tator_api.get_media_count(project, attribute=["test_enum::a"], media_id=[video], force_es=0) == tator_api.get_media_count(project, attribute=["test_enum::a"], media_id=[video], force_es=1)
