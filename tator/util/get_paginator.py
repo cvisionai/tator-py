325077de9c890c583f45b44567a34a5dfd0cb8a0
Hugh Enxing <hugh.enxing@cvisionai.com>
2022-11-17 13:01:17 -0500

    Adds pagination test

--- tator/util/get_paginator.py
+++ tator/util/get_paginator.py
@@ -1,32 +1,66 @@
 from difflib import get_close_matches
 from functools import partial
+import logging
+import re
+
+
+logger = logging.getLogger(__name__)
 
 
 class Paginator:
+    """Manages retrieving pages of results for `get_*_list[_by_id]` API functions"""
+
     def __init__(self, fn, name, batch_size):
-        self.fn = fn
-        self.name = name
-        self.batch_size = batch_size
+        self._fn = fn
+        self._name = name
+        self._batch_size = batch_size
 
     def paginate(self, **kwargs):
-        func = partial(self.fn, **kwargs)
-        page_results = self.batch_size
+        """
+        Passes `kwargs` to the stored function and yields pages of results with (at most)
+        `_batch_size` results.
+        """
+        get_page = partial(self._fn, **kwargs)
+        page_count = self._batch_size
         page_num = 0
-        msg = self.name + " returned page {} ({} cumulative results)."
+        msg = self._name + " returned page {} ({} cumulative results)."
 
-        while page_results == self.batch_size:
-            start = page_num * self.batch_size
-            page = func(start=start, stop=start + self.batch_size)
-            page_results = len(page)
-            logger.info(msg.format(page_num + 1, page_results + start))
+        while page_count == self._batch_size:
+            start = page_num * self._batch_size
+            page = get_page(start=start, stop=start + self._batch_size)
+            page_count = len(page)
+            logger.info(msg.format(page_num + 1, page_count + start))
             page_num += 1
             yield page
 
 
 def get_paginator(api, func_name, batch_size=1000):
+    """Returns a Paginator object that returns pages of results from the given `func_name`.
+
+    Example:
+
+    .. code-block:: python
+
+        import tator
+
+        api = tator.get_api(host, token)
+        media_paginator = tator.util.get_paginator(api, "get_media_list")
+        page_iterator = media_paginator.paginate(project=project_id)
+        all_media = []
+        for page in page_iterator:
+            for media in page:
+                all_media.append(media)
+
+    :param api: :class:`tator.TatorApi` object.
+    :param str func_name:
+    :param int batch_size:
+    :returns: Paginator object
+    """
     try:
         fn = getattr(api, func_name)
     except AttributeError:
         similar = get_close_matches(func_name, dir(api), n=1)[0]
         raise ValueError(f"TatorApi has no function '{func_name}', did you mean '{similar}'?")
+    if not re.search(r"^get_.+_list(_by_id)?$", func_name):
+        raise ValueError(f"Cannot paginate '{func_name}', expected a 'get list' type operation")
     return Paginator(fn, name, batch_size)
---
+++ test/test_pagination.py
@@ -0,0 +1,112 @@
+from types import GeneratorType
+import datetime
+import random
+import uuid
+
+import tator
+
+
+def random_localization(project, box_type, video_obj, post=False):
+    x = random.uniform(0.0, 1.0)
+    y = random.uniform(0.0, 1.0)
+    w = random.uniform(0.0, 1.0 - x)
+    h = random.uniform(0.0, 1.0 - y)
+    attributes = {
+        "test_bool": random.choice([False, True]),
+        "test_int": random.randint(-1000, 1000),
+        "test_float": random.uniform(-1000.0, 1000.0),
+        "test_enum": random.choice(["a", "b", "c"]),
+        "test_string": str(uuid.uuid1()),
+        "test_datetime": datetime.datetime.now().isoformat(),
+        "test_geopos": [random.uniform(-180.0, 180.0), random.uniform(-90.0, 90.0)],
+        "test_float_array": [random.uniform(-1.0, 1.0) for _ in range(3)],
+    }
+    out = {
+        "x": x,
+        "y": y,
+        "width": w,
+        "height": h,
+        "project": project,
+        "type": box_type,
+        "media_id": video_obj.id,
+        "frame": random.randint(0, video_obj.num_frames - 1),
+    }
+    if post:
+        out = {**out, **attributes}
+    else:
+        out["attributes"] = attributes
+    return out
+
+
+def _assert_pagination(api, function_name, batch_size, total, **kwargs):
+    # Get paginator for `get_localization_list`
+    paginator = tator.util.get_paginator(api, function_name, batch_size=batch_size)
+    page_iter = paginator.paginate(**kwargs)
+    assert type(page_iter) == GeneratorType
+
+    # Check the batch size
+    all_objects = []
+    expect_stop_iteration = False
+    while True:
+        try:
+            response = next(page_iter)
+        except StopIteration:
+            assert expect_stop_iteration
+            break
+        else:
+            assert expect_stop_iteration == False
+            all_objects += response
+            if len(response) < batch_size:
+                expect_stop_iteration = True
+            else:
+                assert len(response) == batch_size
+
+    assert len(all_objects) == num_localizations
+
+
+def test_localization_crud(host, token, project, video_type, video, box_type):
+    tator_api = tator.get_api(host, token)
+    video_obj = tator_api.get_media(video)
+
+    # These fields will not be checked for object equivalence after patch.
+    exclude = ["project", "type", "media_id", "id", "meta", "user", "ids"]
+
+    # Test bulk create.
+    batch_size = 100
+    num_localizations = 2222  # Should not be a multiple of batch_size
+    boxes = [
+        random_localization(project, box_type, video_obj, post=True)
+        for _ in range(num_localizations)
+    ]
+    box_ids = []
+    for response in tator.util.chunked_create(
+        tator_api.create_localization_list, project, localization_spec=boxes
+    ):
+        box_ids += response.id
+    assert len(box_ids) == len(boxes)
+    assert len(box_ids) % 100 != 0
+    print(f"Created {len(box_ids)} boxes!")
+
+    # Verify list is the right length
+    response = tator_api.get_localization_list(project, type=box_type)
+    assert len(response) == num_localizations
+
+    # Check `get_localization_list`
+    _assert_pagination(
+        tator_api,
+        "get_localization_list",
+        batch_size,
+        num_localizations,
+        project=project,
+        type=box_type,
+    )
+
+    # Check `get_localization_list_by_id`
+    _assert_pagination(
+        tator_api,
+        "get_localization_list_by_id",
+        batch_size,
+        num_localizations,
+        project=project,
+        localization_id_query={"media_ids": [video]},
+    )
