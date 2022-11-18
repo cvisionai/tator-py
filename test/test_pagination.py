2566a86023da5557b622c8262cdff447561894f4
Hugh Enxing <hugh.enxing@cvisionai.com>
2022-11-18 14:03:04 -0500

    Fixes test

--- test/test_pagination.py
+++ test/test_pagination.py
@@ -6,7 +6,29 @@ import uuid
 import tator
 
 
-def random_localization(project, box_type, video_obj, post=False):
+def make_video(tator_api, project, video_type, video_file, test_string):
+    attributes = {"test_string": test_string}
+    for progress, response in tator.util.upload_media(tator_api, video_type, video_file, attributes=attributes):
+        print(f"Upload video progress: {progress}%")
+    print(response.message)
+    while True:
+        response = tator_api.get_media_list(project, name='AudioVideoSyncTest_BallastMedia.mp4')
+        print("Waiting for transcode...")
+        time.sleep(2.5)
+        if len(response) == 0:
+            continue
+        if response[0].media_files is None:
+            continue
+        streaming = response[0].media_files.streaming
+        have_archival = response[0].media_files.archival is not None
+        if streaming and have_archival and len(streaming) == 4:
+            video_id = response[0].id
+            break
+    assert response[0].attributes.get("test_string") == attributes.get("test_string")
+
+    return video_id
+
+def random_localization(project, box_type, video_obj, test_string, post=False):
     x = random.uniform(0.0, 1.0)
     y = random.uniform(0.0, 1.0)
     w = random.uniform(0.0, 1.0 - x)
@@ -16,7 +38,7 @@ def random_localization(project, box_type, video_obj, post=False):
         "test_int": random.randint(-1000, 1000),
         "test_float": random.uniform(-1000.0, 1000.0),
         "test_enum": random.choice(["a", "b", "c"]),
-        "test_string": str(uuid.uuid1()),
+        "test_string": test_string,
         "test_datetime": datetime.datetime.now().isoformat(),
         "test_geopos": [random.uniform(-180.0, 180.0), random.uniform(-90.0, 90.0)],
         "test_float_array": [random.uniform(-1.0, 1.0) for _ in range(3)],
@@ -64,16 +86,16 @@ def _assert_pagination(api, function_name, batch_size, total, **kwargs):
     assert len(all_objects) == total
 
 
-def test_localization_pagination(host, token, project, video_type, video, box_type):
+def test_localization_pagination(host, token, project, video_type, video_file, box_type):
+    test_string = str(uuid1())
     tator_api = tator.get_api(host, token)
+    video = make_video(tator_api, project, video_type, video_file, test_string)
     video_obj = tator_api.get_media(video)
 
-    existing_localizations = tator_api.get_localization_list(project, type=box_type)
-    n_existing_loc = len(existing_localizations)
     batch_size = 100
     num_localizations = 2222  # Should not be a multiple of batch_size
     boxes = [
-        random_localization(project, box_type, video_obj, post=True)
+        random_localization(project, box_type, video_obj, test_string, post=True)
         for _ in range(num_localizations)
     ]
     box_ids = []
@@ -97,6 +119,7 @@ def test_localization_pagination(host, token, project, video_type, video, box_ty
         num_localizations,
         project=project,
         type=box_type,
+        media_id=[video],
     )
 
     # Check `get_localization_list_by_id`
@@ -108,3 +131,5 @@ def test_localization_pagination(host, token, project, video_type, video, box_ty
         project=project,
         localization_id_query={"media_ids": [video]},
     )
+
+    tator_api.delete_media(video)
