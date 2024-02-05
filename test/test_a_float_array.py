96a4b95eed6385aec051242cd346626a8b39e309
Brian Tate <brian.tate@cvisionai.com>
2024-02-05 15:09:31 -0500

    Fix 'test_a_float_array.py' to work in different test orders

--- test/test_a_float_array.py
+++ test/test_a_float_array.py
@@ -111,7 +111,7 @@ def test_float_array(host, token, project, box_type, video_temp):
     assert(isinstance(response, tator.models.MessageResponse))
 
     # Check we deleted the right boxes.
-    count = api.get_localization_count(project)
+    count = api.get_localization_count(project, type=box_type)
     assert(count == 4)
 
     # Delete remaining localizations.
@@ -125,6 +125,5 @@ def test_float_array(host, token, project, box_type, video_temp):
     assert(isinstance(response, tator.models.MessageResponse))
 
     # Check localizations are gone.
-    count = api.get_localization_count(project)
+    count = api.get_localization_count(project, type=box_type)
     assert(count == 0)
-
