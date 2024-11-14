f564f3de2f817469dbc38e5fb09c8fb975a03448
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2024-11-14 15:50:01 +0000

    Update path to hosted template

--- test/test_hosted_template.py
+++ test/test_hosted_template.py
@@ -6,7 +6,7 @@ def test_algo_template(host, token, organization, project, video):
     api = tator.get_api(host, token)
     response = api.create_hosted_template(organization, {
         "name": "echo",
-        "url": "https://raw.githubusercontent.com/cvisionai/tator/dev/500-hosted-workflows-applets/doc/examples/workflow_template/echo.yaml",
+        "url": "https://raw.githubusercontent.com/cvisionai/tator/dev/fix-concat-duration/doc/examples/workflow_template/echo.yaml",
         "headers": [],
         "tparams": [{"name": "message", "value": "Hello from the tator-py tests"}]
     })
