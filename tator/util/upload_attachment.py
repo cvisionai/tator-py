758888cd9929ff0f64a853fe45f642f56d56f0e1
Brian Tate <brian.tate@cvisionai.com>
2024-11-25 11:53:34 -0500

    Fix default chunk size in upload_attachment

--- tator/util/upload_attachment.py
+++ tator/util/upload_attachment.py
@@ -2,8 +2,9 @@ import os
 
 from ._upload_file import _upload_file
 
-def upload_attachment(api, media, path, name=None, chunk_size=100*1024*1024):
-    """ Upload a file as an attachment to a media object.
+
+def upload_attachment(api, media, path, name=None, chunk_size=1 * 1024 * 1024):
+    """Upload a file as an attachment to a media object.
 
     Example:
 
@@ -18,7 +19,7 @@ def upload_attachment(api, media, path, name=None, chunk_size=100*1024*1024):
     :param media: Unique integer identifying a media.
     :param path: Path to the file.
     :param name: [Optional] Name of file in database. Defaults to basename of path.
-    :param chunk_size: [Optional] Chunk size in bytes. Default is 100MB.
+    :param chunk_size: [Optional] Chunk size in bytes. Default is 10MB.
     :returns: Generator that yields tuple containing progress (0-100) and a
         response. The response is `None` until the last yield, when the response
         is the response object from :meth:`tator.util.TatorApi.create_auxiliary_file`.
@@ -29,8 +30,9 @@ def upload_attachment(api, media, path, name=None, chunk_size=100*1024*1024):
     if name is None:
         name = os.path.basename(path)
 
-    for progress, upload_info in _upload_file(api, media_obj.project, path, media_id=media,
-                                              filename=name):
+    for progress, upload_info in _upload_file(
+        api, media_obj.project, path, media_id=media, filename=name, chunk_size=chunk_size
+    ):
         yield (progress, None)
 
     file_def = {'size': os.stat(path).st_size,
