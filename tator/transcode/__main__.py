be1b61d8f7d8f1153174b35c675f8a44eb1e6c14
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2026-02-11 11:01:44 -0500

    Remove datadog stuff

--- tator/transcode/__main__.py
+++ tator/transcode/__main__.py
@@ -26,14 +26,8 @@ from .transcode import convert_audio
 from .delete_media import delete_media
 from .make_thumbnails import make_thumbnail_image, make_thumbnail_gif
 
-if os.getenv("DD_LOGS_INJECTION"):
-    import ddtrace.auto
-    FORMAT = ('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] '
-              '[dd.service=%(dd.service)s dd.env=%(dd.env)s dd.version=%(dd.version)s dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s] '
-              '- %(message)s')
-else:
-    FORMAT = ('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] '
-              '- %(message)s')
+FORMAT = ('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] '
+          '- %(message)s')
 logging.basicConfig(format=FORMAT)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
